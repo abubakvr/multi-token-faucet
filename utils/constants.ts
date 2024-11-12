9e2eabb6447794c4ab48155b237b89b068ecb91f
sadeeqdev <boskitechh@gmail.com>
2024-11-12 15:06:09 +0100

    fix: update WETH pricefeed

--- utils/constants.ts
+++ utils/constants.ts
@@ -6,7 +6,7 @@ export const WETH_PRICE_FEED = "0xE831DFd1DDFCce163c0B16B35aD2dc411129EC7d"; // 
 
 export const USDC_CB_PRICE_FEED = "0xE831DFd1DDFCce163c0B16B35aD2dc411129EC7d"; // USDC, cbBTC, cbETH
 
-export const ETH_DEFI_PRICE_FEED = "0x2c01212f051A59D88A1361db1E2041896dB4af64"; // USDC, COMP, UNI, AAVE
+export const ETH_DEFI_PRICE_FEED = "0xE831DFd1DDFCce163c0B16B35aD2dc411129EC7d"; // USDC, COMP, UNI, AAVE
 
 export const CHEDDA_PRICE_FEED = "0x4f69E2b5c3a93F33932e0faFAb3B516510aa5ab6"; // CHEDDA
 
@@ -23,48 +23,48 @@ export const tokens = [
     faucetAmount: ethers.parseUnits("2", 6),
     priceFeed: WETH_PRICE_FEED, // USDC is in WETH_PRICE_FEED
   },
-  // {
-  //   address: "0xF6eea61d35B5A1DdCF7071eC7d5F6a62d649143b", // DAI
-  //   amount: ethers.parseUnits("20", 18),
-  //   faucetAmount: ethers.parseUnits("2", 18),
-  //   priceFeed: WETH_PRICE_FEED, // DAI is in WETH_PRICE_FEED
-  // },
-  // {
-  //   address: "0x2F59Dd801e498a4E80454cbf022313eAB7C5d511", // WETH
-  //   amount: ethers.parseUnits("5", 18),
-  //   faucetAmount: ethers.parseUnits("0.5", 18),
-  //   priceFeed: WETH_PRICE_FEED,
-  // },
-  // {
-  //   address: "0xF1cF6113d2f6B44Bffa7C44D82640Db4e721B48a", // cbETH
-  //   amount: ethers.parseUnits("2", 18),
-  //   faucetAmount: ethers.parseUnits("0.2", 18),
-  //   priceFeed: USDC_CB_PRICE_FEED,
-  // },
-  // {
-  //   address: "0x1bf0aeb4C1A1C0896887814d679defcc1325EdE3", // cbBTC
-  //   amount: ethers.parseUnits("0.1", 8),
-  //   faucetAmount: ethers.parseUnits("0.01", 8),
-  //   priceFeed: USDC_CB_PRICE_FEED,
-  // },
-  // {
-  //   address: "0x0414920Dc0C3Bb615A3d8EAA239D55c4258AAae0", // AAVE
-  //   amount: ethers.parseUnits("10", 18),
-  //   faucetAmount: ethers.parseUnits("1", 18),
-  //   priceFeed: ETH_DEFI_PRICE_FEED,
-  // },
-  // {
-  //   address: "0xC58bb755381C43FC8A9505fFa7C44d8737203300", // COMP
-  //   amount: ethers.parseUnits("10", 18),
-  //   faucetAmount: ethers.parseUnits("1", 18),
-  //   priceFeed: ETH_DEFI_PRICE_FEED,
-  // },
-  // {
-  //   address: "0x8166D0DeFb96900075a667FFb099DE8A493A4DfD", // UNI
-  //   amount: ethers.parseUnits("10", 18),
-  //   faucetAmount: ethers.parseUnits("1", 18),
-  //   priceFeed: ETH_DEFI_PRICE_FEED,
-  // },
+  {
+    address: "0xF6eea61d35B5A1DdCF7071eC7d5F6a62d649143b", // DAI
+    amount: ethers.parseUnits("20", 18),
+    faucetAmount: ethers.parseUnits("2", 18),
+    priceFeed: WETH_PRICE_FEED, // DAI is in WETH_PRICE_FEED
+  },
+  {
+    address: "0x2F59Dd801e498a4E80454cbf022313eAB7C5d511", // WETH
+    amount: ethers.parseUnits("5", 18),
+    faucetAmount: ethers.parseUnits("0.5", 18),
+    priceFeed: WETH_PRICE_FEED,
+  },
+  {
+    address: "0xF1cF6113d2f6B44Bffa7C44D82640Db4e721B48a", // cbETH
+    amount: ethers.parseUnits("2", 18),
+    faucetAmount: ethers.parseUnits("0.2", 18),
+    priceFeed: USDC_CB_PRICE_FEED,
+  },
+  {
+    address: "0x1bf0aeb4C1A1C0896887814d679defcc1325EdE3", // cbBTC
+    amount: ethers.parseUnits("0.1", 8),
+    faucetAmount: ethers.parseUnits("0.01", 8),
+    priceFeed: USDC_CB_PRICE_FEED,
+  },
+  {
+    address: "0x0414920Dc0C3Bb615A3d8EAA239D55c4258AAae0", // AAVE
+    amount: ethers.parseUnits("10", 18),
+    faucetAmount: ethers.parseUnits("1", 18),
+    priceFeed: ETH_DEFI_PRICE_FEED,
+  },
+  {
+    address: "0xC58bb755381C43FC8A9505fFa7C44d8737203300", // COMP
+    amount: ethers.parseUnits("10", 18),
+    faucetAmount: ethers.parseUnits("1", 18),
+    priceFeed: ETH_DEFI_PRICE_FEED,
+  },
+  {
+    address: "0x8166D0DeFb96900075a667FFb099DE8A493A4DfD", // UNI
+    amount: ethers.parseUnits("10", 18),
+    faucetAmount: ethers.parseUnits("1", 18),
+    priceFeed: ETH_DEFI_PRICE_FEED,
+  },
 ];
 
 export const tokensToCheck = [
